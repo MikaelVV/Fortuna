912b13f14f758cf89afb7ae7183b7bbe36ec8f67
Mikael <50752113+MikaelVV@users.noreply.github.com>
2024-07-16 00:35:32 +0300

    Updating movement

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -22,79 +22,92 @@ var paused = false
 @onready var pivot := $Pivot
 @onready var camera := $Pivot/Camera3D
 @onready var pause_menu := $Pivot/Camera3D/PauseMenu
-@onready var body := $MeshInstance3D
+@onready var body := $Body
 # Haetaan painovoima projekti asetuksista.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var gravity = 10
 
 func _ready() -> void:
 	HealthBar.max_value = max_health
 	_set_heatlhbar()
 
 func _process(delta):
-	if(navigationAgent.is_navigation_finished()):
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	
+	if(navigationAgent.is_target_reached()):
 		return
 	
-	moveToPosition(delta)
-	pass
-
-#Määritellään pelaajan point & click movement.
-func moveToPosition(delta):
-	var targetPosition = navigationAgent.get_next_path_position()
-	var direction = global_position.direction_to(targetPosition)
+	var new_velocity = movement()
 	
-	if global_transform.origin.is_equal_approx(targetPosition):
-		return
-	faceToDirection(targetPosition)
+	velocity.x = new_velocity.x
+	velocity.z = new_velocity.z
 	
-	velocity = direction * SPEED
 	move_and_slide()
 	
-func faceToDirection(direction):
-	look_at(Vector3(direction.x, global_position.y, direction.z),Vector3.UP)
+
 
 func _input(event):
 	if Input.is_action_just_pressed("MoveToLocation"):
-		var mousePosition = get_viewport().get_mouse_position()
-		var rayLength = 150
-		var fromPosition = camera.project_ray_origin(mousePosition)
-		var toPosition = fromPosition + camera.project_ray_normal(mousePosition) * rayLength
-		var space = get_world_3d().direct_space_state
-		var rayQuery = PhysicsRayQueryParameters3D.new()
+		get_world_pos();
 		
-		rayQuery.from = fromPosition
-		rayQuery.to = toPosition
+func get_world_pos() -> void:
+	var mousePosition = get_viewport().get_mouse_position()
+	var rayLength = 200
+	var fromPosition = camera.project_ray_origin(mousePosition)
+	var toPosition = fromPosition + camera.project_ray_normal(mousePosition) * rayLength
+	var space = get_world_3d().direct_space_state
+	var rayQuery = PhysicsRayQueryParameters3D.new()
 		
-		var result = space.intersect_ray(rayQuery)
+	rayQuery.from = fromPosition
+	rayQuery.to = toPosition
 		
-		if result.is_empty():
-			return
+	var result = space.intersect_ray(rayQuery)
+		
+	if result.is_empty():
+		return
 			
-		print(result)
+	print(result)
 		
-		navigationAgent.target_position = result.position
+	navigationAgent.target_position = result.position
+	
+	look_at_path(result.position)
+	
+	
 
+func movement() -> Vector3:
+	var next_path_position = navigationAgent.get_next_path_position()
+	var current_agent_position = global_position
+	
+	var new_velocity = next_path_position - current_agent_position
+	new_velocity = new_velocity.normalized() * SPEED
+	
+	
+	return new_velocity
+
+func look_at_path(direction : Vector3) -> void:
+	look_at(Vector3(direction.x, global_position.y, direction.z), Vector3.UP)
+	
 # Function "kuuntelee" kaikkia inputteja, joka tässä tapauksessa on hiiri.
 # ja määrittelee hiiren liikkuvuuden kameran kanssa.
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		if event is InputEventMouseMotion: # Alla määrittellään hiiren herkkyys.
-			pivot.rotate_y(-event.relative.x * 0.01)
-			camera.rotate_x(-event.relative.y * 0.01)
-			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60), deg_to_rad(60)) # Tässä limitoidaan pelaajan kameran rotaatiota, että pelaaja ei voi katsoa 360 astetta ympäri.
+	#if event is InputEventMouseButton:
+		#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	#if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		#if event is InputEventMouseMotion: # Alla määrittellään hiiren herkkyys.
+			#pivot.rotate_y(-event.relative.x * 0.01)
+			#camera.rotate_x(-event.relative.y * 0.01)
+			#camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60), deg_to_rad(60)) # Tässä limitoidaan pelaajan kameran rotaatiota, että pelaaja ei voi katsoa 360 astetta ympäri.
 	if event.is_action_pressed("pause"): # ui_cancel tarkoittaa Esc näppäintä.
 		pauseGame()
 		#Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
-func _physics_process(delta) -> void:
+func _physics_process(delta):
 	# Määritellään pelaajan painovoima.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		damage()
+		velocity.y = SPEED
 
 
 
@@ -139,10 +152,10 @@ func _set_heatlhbar() -> void:
 
 func pauseGame():
 	if paused:
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		pause_menu.hide()
 		get_tree().paused = false
 	else:
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		#Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		get_tree().paused = true
 		pause_menu.show()
